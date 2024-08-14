4f0a0937a732d8568075ad1c50264c6c6045bc38
sarathvs-Dev <sarathvs0880@gmail.com>
2024-08-14 22:50:36 +0530

    shadcn added

--- app/page.js
+++ app/page.js
@@ -8,6 +8,9 @@ import myImage from '@/public/myImage.jpg'
 import EducationTimeline from "./_components/EducationTimeline";
 import JobCareer from "./_components/JobCareer";
 import TechStack from "./_components/TechStack";
+import { Button } from "@/components/ui/button";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { ArrowDownAZ, ArrowDownCircle } from "lucide-react";
 
 export default function Home() {
   return (
@@ -44,13 +47,21 @@ export default function Home() {
           {/* <div className="grid grid-cols-1 md:grid-cols-2 "> */}
 
           <div className="flex justify-center items-center gap-8 text-2xl mt-16 text-gray-500">
-          <a href="https://www.linkedin.com/in/sarath-vs-0938a11ab/">  <FaLinkedin className="cursor-pointer" /></a>
-           <a href="https://www.instagram.com/sarath__v.s/?igsh=MWx4aWN4aWlvazRtNA%3D%3D"> <FaInstagram className="cursor-pointer" /></a>
-            <a href="https://github.com/sarathvs-Dev"><FaGithub className="cursor-pointer" /></a>
+            <a href="https://www.linkedin.com/in/sarath-vs-0938a11ab/">  <FaLinkedin className="cursor-pointer hover:text-blue-900" /></a>
+            <a href="https://www.instagram.com/sarath__v.s/?igsh=MWx4aWN4aWlvazRtNA%3D%3D"> <FaInstagram className="cursor-pointer hover:text-pink-800" /></a>
+            <a href="https://github.com/sarathvs-Dev"><FaGithub className="cursor-pointer hover:text-black" /></a>
             <a href="mailto:sarathvs124@example.com">
-  <BiLogoGmail className="cursor-pointer" />
-</a>
+              <BiLogoGmail className="cursor-pointer hover:text-red-700" />
+            </a>
           </div>
+          <div className="flex justify-center items-center pt-20">
+            <a href="./sarathvs.pdf" download>
+              <Button variant="outline" className="text-gray-500 gap-3">
+                <ArrowDownCircle /> Download My Resume
+              </Button>
+            </a>
+          </div>
+
         </div>
         <div>
           <div className="grid md:grid-cols-2 grid-cols-1 gap-28">
