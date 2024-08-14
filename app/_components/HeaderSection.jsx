ccb982540271ea0a89a6dffd1281ff91af01cbc6
sarathvs-Dev <sarathvs0880@gmail.com>
2024-08-14 10:32:58 +0530

    footer

--- app/_components/HeaderSection.jsx
+++ app/_components/HeaderSection.jsx
@@ -1,58 +1,57 @@
-import React from 'react'
+import React from "react";
 
 function HeaderSection() {
   return (
     <div>
-        <div className='bg-gray-100'>
-            <nav className='fixed top-0 bg-white w-full '>
-                <div className='m-auto flex justify-end md:mx-28 md:my-6  my-4 items-center text-gray-700'>
-                    {/* <h1 className='pl-8 py-4 text-xl '>SARATH VS</h1> */}
+      <div className="bg-gray-100">
+        <nav className="fixed top-0 bg-white w-full ">
+          <div className="m-auto flex justify-end md:mx-28 md:my-6  my-4 items-center text-gray-700">
+            {/* <h1 className='pl-8 py-4 text-xl '>SARATH VS</h1> */}
 
-                    <ul className='hidden md:flex items-center pr-10 text-base font-semibold cursor-pointer'>
-                    <li className="relative group py-4 px-6 text-blue-700 font-bold cursor-pointer">
-  Home
-  {/* <span
+            <ul className="hidden md:flex items-center pr-10 text-base font-semibold cursor-pointer">
+              <li className="relative group py-4 px-6 text-blue-700 font-bold cursor-pointer">
+                Home
+                {/* <span
     className="absolute bottom-0  h-[2px] bg-blue-400   transition-all duration-300 ease-in-out w-full left-0"
   ></span> */}
-</li>
+              </li>
 
-<li className="relative group py-4 px-6  hover:text-blue-700 font-bold cursor-pointer">
-  About
-  {/* <span
+              <li className="relative group py-4 px-6  hover:text-blue-700 font-bold cursor-pointer">
+                About
+                {/* <span
     className="absolute bottom-0 left-1/2 w-0 h-[2px] bg-blue-400 transition-all duration-300 ease-in-out group-hover:w-full group-hover:left-0"
   ></span> */}
-</li>
-<li className="relative group py-4 px-6 hover:text-blue-700 font-bold cursor-pointer">
-  Project
-  {/* <span
+              </li>
+              <li className="relative group py-4 px-6 hover:text-blue-700 font-bold cursor-pointer">
+                Project
+                {/* <span
     className="absolute bottom-0 left-1/2 w-0 h-[2px] bg-blue-400 transition-all duration-300 ease-in-out group-hover:w-full group-hover:left-0"
   ></span> */}
-</li>
+              </li>
+            </ul>
+            <button className="block md:hidden py-3 px-4 mx-2 rounded focus:outline-none hover:bg-gray-200 group">
+              <div className="w-5 h-1 bg-gray-600 mb-1"></div>
+              <div className="w-5 h-1 bg-gray-600 mb-1"></div>
+              <div className="w-5 h-1 bg-gray-600 "></div>
 
-
-                    </ul>
-                    <button className='block md:hidden py-3 px-4 mx-2 rounded focus:outline-none hover:bg-gray-200 group'>
-                        <div className='w-5 h-1 bg-gray-600 mb-1'></div>
-                        <div className='w-5 h-1 bg-gray-600 mb-1'></div>
-                        <div className='w-5 h-1 bg-gray-600 '></div>
-
-                        <div className='absolute top-0 -right-full h-screen w-6/12 bg-white border opacity-0
-                        group-focus:right-0 group-focus:opacity-100 transition-all duration-300'>
-                        <ul className='flex flex-col items-center w-full text-base cursor-pointer pt-10'>
-
-                            <li className='hover:bg-gray-200 py-4 px-6 w-full'>Home</li>
-                            <li className='hover:bg-gray-200 py-4 px-6 w-full'>About</li>
-                            <li className='hover:bg-gray-200 py-4 px-6 w-full'>Project</li>
-
-                        </ul>
-                        </div>
-
-                    </button>
-                </div>
-            </nav>
-        </div>
+              <div
+                className="absolute top-0 -right-full h-screen w-6/12 bg-white border opacity-0
+                        group-focus:right-0 group-focus:opacity-100 transition-all duration-300"
+              >
+                <ul className="flex flex-col items-center w-full text-base cursor-pointer pt-10">
+                  <li className="hover:bg-gray-200 py-4 px-6 w-full">Home</li>
+                  <li className="hover:bg-gray-200 py-4 px-6 w-full">About</li>
+                  <li className="hover:bg-gray-200 py-4 px-6 w-full">
+                    Project
+                  </li>
+                </ul>
+              </div>
+            </button>
+          </div>
+        </nav>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeaderSection
+export default HeaderSection;
--- app/page.js
+++ app/page.js
@@ -7,34 +7,34 @@ import FooterSection from "./_components/FooterSection";
 
 export default function Home() {
   return (
-  <>
-  <div className="font-roboto">
+    <>
+      <div className="font-roboto">
 
- 
-   <div className="my-52">
-        <h2 className=" text-3xl flex justify-center items-center md:text-5xl font-roboto">
-          Software developer
-        </h2>
-        <p className="flex justify-center items-center md:px-32 md:pt-5 font-roboto from-neutral-400">
-          I’m Nandhu Sathish, a fullstack developer and UI/UX designer based in
-          Kerala, India.Experienced software developer with expertise in
-          designing, developing, and maintaining software systems. Proficient in
-          various platforms, languages, and frameworks, including React,
-          Next.Js, Angular, and Express JS.
-        </p>
 
-        <div className="flex justify-center items-center gap-8 text-2xl mt-16 text-gray-500">
-          <FaLinkedin className="cursor-pointer" />
-          <FaInstagram className="cursor-pointer" />
-          <FaGithub className="cursor-pointer" />
-          <BiLogoGmail className="cursor-pointer" />
+        <div className="my-52">
+          <h2 className=" text-3xl flex justify-center items-center md:text-5xl font-roboto">
+            Software developer
+          </h2>
+          <p className="flex justify-center items-center md:px-32 md:pt-5 px-8 font-roboto from-neutral-400">
+            I’m Nandhu Sathish, a fullstack developer and UI/UX designer based in
+            Kerala, India.Experienced software developer with expertise in
+            designing, developing, and maintaining software systems. Proficient in
+            various platforms, languages, and frameworks, including React,
+            Next.Js, Angular, and Express JS.
+          </p>
+
+          <div className="flex justify-center items-center gap-8 text-2xl mt-16 text-gray-500">
+            <FaLinkedin className="cursor-pointer" />
+            <FaInstagram className="cursor-pointer" />
+            <FaGithub className="cursor-pointer" />
+            <BiLogoGmail className="cursor-pointer" />
+          </div>
+        </div>
+        <div>
+          <FooterSection />
         </div>
       </div>
-      <div>
-       <FooterSection/>
-      </div>
-      </div>
-      </>
-      
+    </>
+
   );
 }
