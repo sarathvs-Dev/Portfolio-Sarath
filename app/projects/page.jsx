02b038837bddbf4f2f7742d64b71659441e33d0b
sarathvs-Dev <sarathvs0880@gmail.com>
2024-08-15 16:44:24 +0530

    project card

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import HeroSection from './_components/HeroSection'
+import ProjectCard from './_components/ProjectCard'
 
 function Project() {
   return (
     <div className='my-36'>
         
         <HeroSection/>
+        <div className='my-12 gap-6 grid grid-cols-3'>
+        <ProjectCard/>
+        <ProjectCard/>
+        <ProjectCard/>
+
+        </div>
     </div>
   )
 }
