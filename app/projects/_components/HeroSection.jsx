bb7e8b6d109135d0fb59cb954c3bcdbf32798820
sarathvs-Dev <sarathvs0880@gmail.com>
2024-08-15 16:25:25 +0530

    project

---
+++ app/projects/_components/HeroSection.jsx
@@ -0,0 +1,20 @@
+import { Separator } from '@/components/ui/separator'
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div>
+        
+        <div className='flex flex-col justify-start items-center mx-10 gap-10'>
+
+            <h2 className='font-bold text-5xl mr-96 ml-16'>Things I’ve made trying to put my mark in the digital universe.</h2>
+
+            <h2 className='mr-96 ml-16'>I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved.</h2>
+
+            <Separator/> 
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import HeroSection from './_components/HeroSection'
 
 function Project() {
   return (
-    <div className='my-36'>page</div>
+    <div className='my-36'>
+        
+        <HeroSection/>
+    </div>
   )
 }
 
