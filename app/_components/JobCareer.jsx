10cff16d424408763d42e3d0259dde08893359b7
sarathvs-Dev <sarathvs0880@gmail.com>
2025-02-11 20:37:41 +0530

    chnagees

--- app/_components/JobCareer.jsx
+++ app/_components/JobCareer.jsx
@@ -11,6 +11,19 @@ function JobCareer() {
     <div class="w-80 -z-20">
       <h2 class="text-xl text-gray-900 mb-7 flex items-center gap-3 font-bold"><BriefcaseBusiness />Work Experience</h2>
       <ul>
+      <li class="relative flex items-baseline gap-6 pb-5">
+          <div class="before:absolute before:left-[5.5px] before:h-full before:w-[1px] before:bg-gray-400 ">
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="bi bi-circle-fill fill-gray-400" viewBox="0 0 16 16">
+              <circle cx="8" cy="8" r="8" />
+            </svg>
+          </div>
+          <div>
+            <p class="text-sm text-gray-600 font-semibold">Software Engineer</p>
+            <p class="mt-2 text-gray-600 text-sm">Teksalah,Kochi</p>
+            <p class="mt-2 text-gray-600 text-sm">2024 - Present</p>
+
+          </div>
+        </li>
         <li class="relative flex items-baseline gap-6 pb-5">
           <div class="before:absolute before:left-[5.5px] before:h-full before:w-[1px] before:bg-gray-400 ">
             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" class="bi bi-circle-fill fill-gray-400" viewBox="0 0 16 16">
